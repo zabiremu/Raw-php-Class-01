56c000bf56afbadb2044e501d0deb33b87047494
zabiremu <zabirraihan@gmail.com>
2022-06-28 12:44:22 +0600

    raw php class-01

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+/*  Statement   */
+echo "hello World <br>";
+echo "<br>";
+/*  statement   */
+ echo "<h1> I am zabir raihan </h1>";
+ echo "<br>";
+ /**
+  * variable rule
+ *  1. store up a value
+ *  2. value dynamic
+ */
+ $name = "I am zabir raihan. My age is ";
+ echo $name;
+ echo "<br>";
+ echo "<br>";
+// /*  statement   */
+ $age = 24;
+ echo $name, $age;
+ echo "<br>";
+ echo "<br>";
+  /*  statement   */
+ $userName = "Zabir Emu. ";
+ $age1 = 24;
+ echo 'My name is '. $userName . 'I am '. $age1 . ' years old. <br>';
+ echo "<br>";
+ echo "My name is ${userName} My age is ${age1}.";
+ echo "<br>";
+ echo "<br>";
+// /*  arithmetic operators   */
+ $x = 200;
+ $y = 20;
+ echo $x + $y;
+ echo "<br>";
+ echo "<br>";
+ echo $x - $y;
+ echo "<br>";
+ echo "<br>";
+ echo $x * $y;
+ echo "<br>";
+ echo "<br>";
+ echo $x / $y;
+ echo "<br>";
+ echo "<br>";
+ echo $x % $y;
+?>
